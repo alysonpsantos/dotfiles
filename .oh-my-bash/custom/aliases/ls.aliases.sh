5478ceccc2b017b0c15ebdb0bf8110e2d5c59862
businessman <empty@empty.empty>
2023-04-26 19:56:59 -0300

    Add .oh-my-bash/custom/ files

---
+++ .oh-my-bash/custom/aliases/ls.aliases.sh
@@ -0,0 +1,5 @@
+# Add your own custom alias in the custom/aliases directory. Aliases placed
+# here will override ones with the same name in the main alias directory.
+# Override ls.aliases.sh
+
+alias la='ls -A'
